f9d8ba4ae496f59486faed85a436125c2ed1197c
jgjfuture <abovethechicken@outlook.jp>
2023-08-03 17:09:22 +0000

    add content header

--- apply.js
+++ apply.js
@@ -3,6 +3,23 @@ import { notion } from "./notion.js"
 export async function apply(id, children) {
     await notion.blocks.children.append({
         block_id: id,
-        children: children
+        children: [makeHeddingBlock("WRITING PIPELINES RESULT"), ...children]
     });
+}
+
+function makeHeddingBlock(text) {
+    return {
+        object: 'block',
+        type: 'heading_2',
+        heading_2: {
+            rich_text: [
+                {
+                    type: 'text',
+                    text: {
+                        content: text,
+                    },
+                },
+            ]
+        }
+    }
 }
